7dd85b562301d9c510a9d9725cb20a7e8edf3cb2
Andrija <akinovak@gmail.com>
2022-06-23 19:20:15 +0200

    nomenclature fix

--- src/lib.rs
+++ src/lib.rs
@@ -16,20 +16,15 @@ use pedersen::unsafe_hash_to_field;
 use signature::{parameters, private_key_to_public_key, sign as starknet_sign};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub struct PulicKey {
+pub struct PublicKey {
     x: Vec<u8>,
     y: Vec<u8>,
 }
 
 #[wasm_bindgen]
-impl PulicKey {
+impl PublicKey {
     #[wasm_bindgen(constructor)]
     pub fn new(x: Vec<u8>, y: Vec<u8>) -> Self {
         Self { x, y }
@@ -78,7 +73,7 @@ impl StarknetModule {
         Self { private_key: None }
     }
 
-    pub fn load_pk(&mut self, private_key: Vec<u8>) {
+    pub fn load_sk(&mut self, private_key: Vec<u8>) {
         self.private_key = Some(private_key.clone())
     }
 
@@ -87,14 +82,14 @@ impl StarknetModule {
         Ok(Uint8Array::from(&pk_bytes[..]))
     }
 
-    pub fn get_public_key(&self) -> Result<PulicKey, JsValue> {
+    pub fn get_public_key(&self) -> Result<PublicKey, JsValue> {
         let pk_bytes = self.private_key.clone().expect("No private key provided");
         let private_key = Fr::from_be_bytes_mod_order(pk_bytes.as_slice());
 
         let parameters = parameters();
         let public_key = private_key_to_public_key(&parameters, private_key).into_affine();
 
-        Ok(PulicKey::new(
+        Ok(PublicKey::new(
             public_key.x.into_repr().to_bytes_be(),
             public_key.y.into_repr().to_bytes_be(),
         ))
@@ -116,10 +111,10 @@ impl StarknetModule {
         ))
     }
 
-    pub fn sign_with_pk(&self, pk_bytes: Vec<u8>, msg: &str) -> Result<Signature, JsValue> {
+    pub fn sign_with_external_sk(&self, private_key_bytes: Vec<u8>, msg: &str) -> Result<Signature, JsValue> {
         let parameters = parameters();
 
-        let private_key = Fr::from_be_bytes_mod_order(pk_bytes.as_slice());
+        let private_key = Fr::from_be_bytes_mod_order(private_key_bytes.as_slice());
         let msg_hash = unsafe_hash_to_field(msg.as_bytes()).expect("Hash failed");
         let sig = starknet_sign(&parameters, private_key, msg_hash, None)
             .expect("Message is out of bound");
--- www/index.js
+++ www/index.js
@@ -4,7 +4,7 @@ import { bufToBigint } from 'bigint-conversion'
 const starknet = new StarknetModule(); 
 
 const private_key = Uint8Array.from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5])
-starknet.load_pk(private_key)
+starknet.load_sk(private_key)
 console.log('private key: ', bufToBigint(starknet.get_private_key()))
 
 const pk = starknet.get_public_key();
@@ -15,6 +15,6 @@ const signature1 = starknet.sign("hello hackers!")
 console.log('r1: ', bufToBigint(signature1.get_r()))
 console.log('s1: ', bufToBigint(signature1.get_s()))
 
-const signature2 = starknet.sign_with_pk(private_key, "hello hackers!")
+const signature2 = starknet.sign_with_external_sk(private_key, "hello hackers!")
 console.log('r2: ', bufToBigint(signature2.get_r()))
 console.log('s2: ', bufToBigint(signature2.get_s()))
