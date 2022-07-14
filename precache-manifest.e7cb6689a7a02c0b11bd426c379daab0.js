1e263cbe734744a92d0f336c5b28b194acf73466
Andrija <akinovak@gmail.com>
2022-07-14 17:49:10 +0200

    Updates

---
+++ precache-manifest.e7cb6689a7a02c0b11bd426c379daab0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15117c47103b3a77fd6",
+    "url": "/85510ae11d941d21aef3.module.wasm"
+  },
+  {
+    "revision": "14f7d14d98cc169b8c408e841c07c0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1bf2a8c4d1c47c741e1",
+    "url": "/static/js/2.53681af3.chunk.js"
+  },
+  {
+    "revision": "e19a091c477b3ce4f156ce4f04aae426",
+    "url": "/static/js/2.53681af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15117c47103b3a77fd6",
+    "url": "/static/js/3.9755b27f.chunk.js"
+  },
+  {
+    "revision": "dd6d3f4d0a9bdd50ac8e",
+    "url": "/static/js/main.0ef28abc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0ef28abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db038130816488b0fd2",
+    "url": "/static/js/runtime-main.edb5da2f.js"
+  }
+]);
