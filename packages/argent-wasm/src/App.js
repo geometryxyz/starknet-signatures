dbbef90200ecc9f19715856cce0564268746cbfa
Marcello Bardus <marcello@bardus.it>
2022-07-19 16:58:56 +0200

    argent x integration

--- packages/argent-wasm/src/App.js
+++ packages/argent-wasm/src/App.js
@@ -1,15 +1,24 @@
-import { StarknetProvider, getInstalledInjectedConnectors, useStarknet, useConnectors } from '@starknet-react/core';
-import { toBufferLE, toBigIntLE } from 'bigint-buffer';
+import {
+  StarknetProvider,
+  getInstalledInjectedConnectors,
+  useStarknet,
+  useConnectors,
+  useStarknetInvoke,
+  useContract,
+} from "@starknet-react/core";
+import { toBufferLE, toBigIntLE } from "bigint-buffer";
 import * as toBuffer from "typedarray-to-buffer";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useBetween } from "use-between";
 
-import {starknet} from "./";
+import VerifySigAbi from "./abi/contract.json";
+
+import { starknet } from "./";
 
 const BUFF_LEN = 32;
 
 function isNumeric(value) {
-	return /^-?\d+$/.test(value);
+  return /^-?\d+$/.test(value);
 }
 
 const useFormState = () => {
@@ -22,7 +31,18 @@ const useFormState = () => {
   const [feltsToSign, setFelts] = useState([]);
 
   return {
-    secretKey, setSecretKey, public_key_x, setPkX, public_key_y, setPkY, feltsToSign, setFelts, sig_x, setSigX, sig_y, setSigY
+    secretKey,
+    setSecretKey,
+    public_key_x,
+    setPkX,
+    public_key_y,
+    setPkY,
+    feltsToSign,
+    setFelts,
+    sig_x,
+    setSigX,
+    sig_y,
+    setSigY,
   };
 };
 
@@ -32,137 +52,177 @@ const SKGeneratorComponent = () => {
   const { setSecretKey, setPkX, setPkY } = useSharedFormState();
   return (
     <div>
-      <button onClick={() => {
-        const private_key = 5n;
-        starknet.load_sk(toBufferLE(private_key, BUFF_LEN));
-        const pk = starknet.get_public_key()
-        setPkX(toBigIntLE(toBuffer(pk.get_x())));
-        setPkY(toBigIntLE(toBuffer(pk.get_y())));
-        setSecretKey(private_key)
-      }}>
+      <button
+        onClick={() => {
+          const private_key = 5n;
+          starknet.load_sk(toBufferLE(private_key, BUFF_LEN));
+          const pk = starknet.get_public_key();
+          setPkX(toBigIntLE(toBuffer(pk.get_x())));
+          setPkY(toBigIntLE(toBuffer(pk.get_y())));
+          setSecretKey(private_key);
+        }}
+      >
         Generate new Private Key
       </button>
     </div>
   );
-}
+};
 
 const PKDisplayComponent = () => {
   const { public_key_x, public_key_y, secretKey } = useSharedFormState();
 
-  return(
+  return (
     <div>
-      sk:  {secretKey ? secretKey.toString() : "Empty" } <br></br>
-      pk_x:  {public_key_x ? public_key_x.toString() : "Empty" } <br></br>
-      pk_y:  {public_key_y ? public_key_y.toString() : "Empty" } <br></br>
+      sk: {secretKey ? secretKey.toString() : "Empty"} <br></br>
+      pk_x: {public_key_x ? public_key_x.toString() : "Empty"} <br></br>
+      pk_y: {public_key_y ? public_key_y.toString() : "Empty"} <br></br>
     </div>
-  )
-}
+  );
+};
 
 const InputComponent = () => {
   const { setFelts } = useSharedFormState();
 
   const [message, setMessage] = useState([]);
-	const [inputSize, setInputSize] = useState(0);
-	const [currentFelt, setCurrentFelt] = useState('');
-  
+  const [inputSize, setInputSize] = useState(0);
+  const [currentFelt, setCurrentFelt] = useState("");
+
   return (
     <div>
       <table>
-				<td key={0}>
-					<th>Message</th>
-				</td>
-				{new Array(inputSize).fill(0).map((e, i) => (
-					<td key={i + 1}>
-						<th>Felt {message[i]}</th>
-					</td>
-				))}
-				<div>
-					<input
-						onChange={(e) => {
-							const input = e.target.value;
-							setCurrentFelt(input);
-						}}
-					/>
-					<button
-						onClick={() => {
-							if (isNumeric(currentFelt)) {
-								setMessage([...message, currentFelt]);
-								setInputSize(inputSize + 1);
-                setFelts([...message, currentFelt])
-							}
-						}}
-						disabled={!isNumeric(currentFelt)}
-					>
-						confirm
-					</button>
-				</div>
-			</table>
+        <td key={0}>
+          <th>Message</th>
+        </td>
+        {new Array(inputSize).fill(0).map((e, i) => (
+          <td key={i + 1}>
+            <th>Felt {message[i]}</th>
+          </td>
+        ))}
+        <div>
+          <input
+            onChange={(e) => {
+              const input = e.target.value;
+              setCurrentFelt(input);
+            }}
+          />
+          <button
+            onClick={() => {
+              if (isNumeric(currentFelt)) {
+                setMessage([...message, currentFelt]);
+                setInputSize(inputSize + 1);
+                setFelts([...message, currentFelt]);
+              }
+            }}
+            disabled={!isNumeric(currentFelt)}
+          >
+            confirm
+          </button>
+        </div>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 const SignComponent = () => {
   const { feltsToSign, setSigX, setSigY } = useSharedFormState();
   return (
     <div>
-      <button onClick={() => {
-        const felts_le = feltsToSign.map((felt) => toBufferLE(felt, BUFF_LEN));
-        const signature = starknet.sign(felts_le)
+      <button
+        onClick={() => {
+          const felts_le = feltsToSign.map((felt) =>
+            toBufferLE(felt, BUFF_LEN)
+          );
+          const signature = starknet.sign(felts_le);
 
-        setSigX(toBigIntLE(toBuffer(signature.get_r())));
-        setSigY(toBigIntLE(toBuffer(signature.get_s())));
-        
-      }}>Sign</button>
+          setSigX(toBigIntLE(toBuffer(signature.get_r())));
+          setSigY(toBigIntLE(toBuffer(signature.get_s())));
+        }}
+      >
+        Sign
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const SignatureDisplayComponent = () => {
   const { sig_x, sig_y } = useSharedFormState();
 
-  return(
+  return (
     <div>
-      sig_x:  {sig_x ? sig_x.toString() : "" } <br></br>
-      sig_y:  {sig_y ? sig_y.toString() : "" } <br></br>
+      sig_x: {sig_x ? sig_x.toString() : ""} <br></br>
+      sig_y: {sig_y ? sig_y.toString() : ""} <br></br>
     </div>
-  )
-}
+  );
+};
 
 const WalletComponent = () => {
-    const { connect, connectors } = useConnectors()
-    return (
-      <div>
-        {connectors.map((connector) =>
-          connector.available() ? (
-            <button key={connector.id()} onClick={() => connect(connector)}>
-              Connect {connector.name()}
-            </button>
-          ) : null
-        )}
-      </div>
-    )
-}
+  const { connect, connectors } = useConnectors();
+  return (
+    <div>
+      {connectors.map((connector) =>
+        connector.available() ? (
+          <button key={connector.id()} onClick={() => connect(connector)}>
+            Connect {connector.name()}
+          </button>
+        ) : null
+      )}
+    </div>
+  );
+};
+
+const VerifySignatureComponent = () => {
+  const contractAddress =
+    "0x026c8bc8bf071a54c4b0713ad52715fe92a471f85bf7f224322cbb0a29666ce1";
+  const contract = useContract({
+    abi: VerifySigAbi,
+    address: contractAddress,
+  });
+  const method = "verify_signature";
+  const { invoke } = useStarknetInvoke({
+    contract,
+    method,
+  });
+  const { sig_x, sig_y, feltsToSign, public_key_x } = useSharedFormState();
+
+  const onClick = async () => {
+    const res = await invoke({
+      args: [feltsToSign.length, ...feltsToSign, public_key_x, [sig_x, sig_y]],
+      metadata: {
+        method: "verifySignature",
+        message: "verifying signature",
+      },
+    });
+    console.log(res);
+  };
+
+  return (
+    <button onClick={onClick} disabled={!sig_x && !sig_y}>
+      Verify on starknet
+    </button>
+  );
+};
 
 const AccComponent = () => {
-    const { account } = useStarknet()
-  
-    return <div>Active account: {account}</div>
-}
+  const { account } = useStarknet();
+
+  return <div>Active account: {account}</div>;
+};
 
 const App = () => {
-    const connectors = getInstalledInjectedConnectors()
+  const connectors = getInstalledInjectedConnectors();
 
-    return (
-      <StarknetProvider connectors={connectors}>
-        <WalletComponent />
-        <AccComponent />
-        <SKGeneratorComponent/>
-        <PKDisplayComponent/>
-        <InputComponent/>
-        <SignComponent></SignComponent>
-        <SignatureDisplayComponent></SignatureDisplayComponent>
-      </StarknetProvider>
-    )
-}
+  return (
+    <StarknetProvider connectors={connectors}>
+      <WalletComponent />
+      <AccComponent />
+      <SKGeneratorComponent />
+      <PKDisplayComponent />
+      <InputComponent />
+      <SignComponent></SignComponent>
+      <SignatureDisplayComponent />
+      <VerifySignatureComponent />
+    </StarknetProvider>
+  );
+};
 
 export default App;
